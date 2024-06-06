9a638036cae89448f9b59d9003d3494ca3ff190a
ValSua <109840298+ValSua@users.noreply.github.com>
2024-06-06 01:07:01 -0500

    se inicia ventana de registro

---
+++ app/src/main/java/com/example/equipodos/view/RegistroActivity.kt
@@ -0,0 +1,12 @@
+package com.example.equipodos.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.equipodos.R
+
+class RegistroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+    }
+}
